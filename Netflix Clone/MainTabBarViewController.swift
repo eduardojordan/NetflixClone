9f04c2ef735e2ad1102a372d544b74005dccb4e3
Eduardo Jordan <eduardojordan@hotmail.com>
2025-03-05 13:17:56 -0500

    refactor data

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -11,13 +11,23 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        setupBackground()
+        SetupTabBar()
+      
+
+    }
+
+    func setupBackground() {
         view.backgroundColor = .systemYellow
-        
+    }
+    
+    func SetupTabBar() {
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: UpcomingViewController())
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
-        
+
         vc1.tabBarItem.image = UIImage(systemName: "house")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
@@ -27,11 +37,10 @@ class MainTabBarViewController: UITabBarController {
         vc2.title = "Coming Soon"
         vc3.title = "Tap Search"
         vc4.title = "Downloads"
-        
-        
         tabBar.tintColor = .label
         
         setViewControllers([vc1,vc2,vc3,vc4], animated: true)
+        
     }
 
 
