eacf68f89cbfcac7edee725d6cccb7c636ac4838
Eduardo Jordan <eduardojordan@hotmail.com>
2025-03-05 13:12:30 -0500

    add tabBarItems

--- Netflix Clone/Controllers/DownloadsViewController.swift
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -12,7 +12,7 @@ class DownloadsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemOrange
+        view.backgroundColor = .systemBackground
     }
     
 
