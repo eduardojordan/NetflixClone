407a825303b38729ea1c012658ab682a37c76e54
Eduardo Jordan <eduardojordan@hotmail.com>
2025-03-10 16:09:16 -0400

    searchView + result search + add query in API

---
+++ Netflix Clone/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Eduardo Jordan on 10/3/25.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
