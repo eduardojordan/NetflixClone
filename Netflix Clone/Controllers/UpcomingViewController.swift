1bf08f6292aba3d6ea99e99a85fba43698be280e
Eduardo Jordan <eduardojordan@hotmail.com>
2025-03-10 22:40:54 -0400

    add preview in the other views

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -16,7 +16,7 @@ class UpcomingViewController: UIViewController {
         table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
         return table
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
@@ -42,17 +42,17 @@ class UpcomingViewController: UIViewController {
         APICaller.shared.getUpcomingMovies { [weak self] result in
             switch result {
             case .success(let titles):
-            self?.titles = titles
+                self?.titles = titles
                 DispatchQueue.main.async {
-           self?.upcomingTable.reloadData()
+                    self?.upcomingTable.reloadData()
                 }
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
     }
-
-
+    
+    
 }
 extension UpcomingViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -70,6 +70,29 @@ extension UpcomingViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_title ?? title.original_name else {
+            return
+        }
+        
+        APICaller.shared.getMovie(with: titleName) { [weak self]resul in
+            switch resul {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            case .failure(let error ):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
